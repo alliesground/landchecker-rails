dc0e1aac4120541fdfffd0f0d8cbd080116fa472
alliesground <sameer.libang@gmail.com>
2021-03-08 22:49:34 +1100

    configuring CORS

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,16 @@ module LandcheckerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000', 'localhost:3001'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+        )
+      end
+    end
   end
 end
